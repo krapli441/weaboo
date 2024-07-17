f70fbd3cc4852a156a9906b5893b76b715ffc0d5
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-17 16:33:20 +0900

    morgan added

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 import express from "express";
+import morgan from "morgan";
+
+const PORT = 3000;
+
+const app = express();
+const loggerMiddleWare = morgan("dev");
+
+const home = (req, res) => {
+  return res.send("boo boo weaboo");
+};
+
+app.use(loggerMiddleWare);
