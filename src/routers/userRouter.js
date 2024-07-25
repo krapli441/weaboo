616aff19183e332f86d86a1f6443d87bca0c5c1f
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-25 16:44:03 +0900

    organ

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { edit } from "../controllers/userControllers";
-import { deleteUser } from "../controllers/userControllers";
+import { edit, remove } from "../controllers/userControllers";
 
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
-userRouter.get("/delete", deleteUser);
+userRouter.get("/delete", remove);
 
 export default userRouter;
