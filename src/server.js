02977b2c3a36d90ed8b59e9caecd42ddece4a4e4
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-25 15:24:42 +0900

    importing routers

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 3000;
 
