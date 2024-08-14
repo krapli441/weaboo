121f909bce78a5ac44daf643a020c021deb48510
박준형 <108733795+krapli441@users.noreply.github.com>
2024-08-14 17:14:26 +0900

    router 추가

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,4 +2,6 @@ export const join = (req, res) => res.send("Join");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Logout");
 export const search = (req, res) => res.send("Search");
+export const see = (req, res) => res.send("See User");
