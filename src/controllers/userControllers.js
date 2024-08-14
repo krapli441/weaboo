8fd5fc50511be2accc0d0ba6bcb8f58197730e59
박준형 <108733795+krapli441@users.noreply.github.com>
2024-08-14 16:40:17 +0900

    userController에 라우터 추가

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,3 +1,5 @@
 export const join = (req, res) => res.send("Join");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
+export const login = (req, res) => res.send("Login");
+export const search = (req, res) => res.send("Search");
