dc41762dfa654c415c3378483e77b0b34fd0f257
박준형 <108733795+krapli441@users.noreply.github.com>
2024-08-22 16:37:15 +0900

    pug 수정

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.send("Watch");
+export const see = (req, res) => res.render("home", { pageTitle: "Watch" });
 export const edit = (req, res) => res.send("Edit");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
